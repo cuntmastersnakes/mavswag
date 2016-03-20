9f6bcf60c1c9967e3957a1d3a72fd8029dfa5dd5
Jonathan Hoogvliet <j.m.hoogvliet@student.tudelft.nl>
2016-03-20 17:17:34 +0100

    Distances are being read by the monocular camera. Ouput yet undefined sadly :(

--- Histogram approach/HistogramBackprojection/historiGrammical.cpp
+++ Histogram approach/HistogramBackprojection/historiGrammical.cpp
@@ -1,4 +1,4 @@
-// HistogramBackprojection.cpp Defines the entry point to the application
+// historiGrammical.cpp Defines the entry point to the application
 //
 
 #include "stdafx.h"
@@ -14,14 +14,61 @@
 using namespace cv;
 using namespace std;
 
+/// Initialization variables, global variable definition
 Mat image, blurred, threshed, closed;
 
 int trackInit = 0;		// Variable to initialize the histogram region
 /// Definition of the reference area
 Rect selection;
 Point lefttop, rightbottom;				// corner points of the reference area
-int refWidth = 60, refHeight = 20;		// Percentage of the screen as reference
-int vmin = 10, vmax = 256, smin = 30;
+int refWidth = 50, refHeight = 10;		// Percentage of the screen as reference
+int vmin = 10, vmax = 256, smin = 30;	// threshold values for hue, value, saturation
+
+/// Section on settings for the distance search
+/// Blocks are vertically run over a number of columns in the image
+/// If the sum of 'ones' in an area reaches a threshold, the box is returned
+Rect block;				// block that searches for the threshold area value
+int numCols = 16;		// number of cols for distance search
+int numRows = 10;		// number of rows for distance search
+float distThresh = 0.5;	// percentage of area of cube in column that needs to be filled
+
+int loopFrame = 0;		// keeps track of internal loopings
+int distances[16]; // Initialize the array for the output
+
+Rect distFind(Mat image,int frame)
+{
+	int stepSize = image.rows / numRows;
+	int stepSizeHor = image.cols / numCols;
+
+	for (int i = image.rows; i > 0; i-= stepSize)
+	{
+		Point samplePoint1, samplePoint2; // lefttop and rightbottom of distance box
+		samplePoint1.x = frame*stepSizeHor; samplePoint1.y = i-stepSize;
+		samplePoint2.x = (frame+1)*stepSizeHor; 
+		samplePoint2.y = i;
+
+		Mat imageROI;
+		Rect searchSpace = Rect(samplePoint1.x, samplePoint1.y,
+			samplePoint2.x - samplePoint1.x, samplePoint2.y - samplePoint1.y);
+		imageROI = image(searchSpace);
+
+		int noOfZeros = countNonZero(imageROI);
+		
+		if (noOfZeros < searchSpace.area()*distThresh)
+		{
+			return searchSpace;
+			break;
+		}
+		else
+		{
+			if (i == 0)
+			{
+				return searchSpace;
+			}
+		}
+
+	}
+}
 
 
 int main(int argc, const char** argv)
@@ -43,10 +90,13 @@ int main(int argc, const char** argv)
 	/// Creation of the windows and the trackbars
 	namedWindow("Histogram", 0);
 	namedWindow("CamShift Demo", 0);
+	namedWindow("Trackbar fiddlin'", 0);
 
-	createTrackbar("Width", "CamShift Demo", &refWidth, 100, 0);
-	createTrackbar("Height", "CamShift Demo", &refHeight, 100, 0);
-	createTrackbar("Smin", "CamShift Demo", &smin, 256, 0);
+	createTrackbar("Vmin", "Trackbar fiddlin'", &vmin, 256, 0);
+	createTrackbar("Vmax", "Trackbar fiddlin'", &vmax, 256, 0);
+	createTrackbar("Smin", "Trackbar fiddlin'", &smin, 256, 0);
+	createTrackbar("Width", "Trackbar fiddlin'", &refWidth, 100, 0);
+	createTrackbar("Height", "Trackbar fiddlin'", &refHeight, 100, 0);
 
 	/// Reserve space for the different image representions and histograms
 	Mat frame, hsv, hue, mask, hist, histimg = Mat::zeros(200, 320, CV_8UC3), backproj;
@@ -61,7 +111,7 @@ int main(int argc, const char** argv)
 		frame.copyTo(image);
 
 		resize(image, blurred, Size(image.cols / 2, image.rows / 2));
-		GaussianBlur(blurred, image, Size(7, 7), 10);
+		GaussianBlur(blurred, image, Size(5, 5), 5);
 		cvtColor(image, hsv, COLOR_BGR2HSV);
 
 		/// Initialize the histogram calculations	
@@ -82,7 +132,7 @@ int main(int argc, const char** argv)
 		rightbottom.y = image.rows;
 
 		selection = Rect(lefttop.x, lefttop.y, rightbottom.x-lefttop.x, rightbottom.y-lefttop.y);
-
+		
 		rectangle(image, lefttop, rightbottom, CV_RGB(255, 0, 0));
 		
 		///To be closer to the Nourbaksh implementation, the intensity
@@ -108,28 +158,37 @@ int main(int argc, const char** argv)
 				Scalar(buf.at<Vec3b>(i)), -1, 8);
 		}
 		
-
+		/// Calculate the black and white result
 		calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
 		backproj &= mask;
-		threshold(backproj, threshed, 127, 255, 0);
+		//threshold(backproj, threshed, 127, 255, 0);
 		Mat element = getStructuringElement(0, Size(9, 9));
-		morphologyEx(threshed, closed, 1, element);
+		morphologyEx(backproj, closed, 1, element);
 		morphologyEx(closed, backproj, 0, element);
-		///Here is where the backprojection image is created
-		RotatedRect trackBox = CamShift(backproj, trackWindow,
-			TermCriteria(TermCriteria::EPS | TermCriteria::COUNT, 10, 1));
-		if (trackWindow.area() <= 1)
+
+		/// Find distances in the frame
+		block = distFind(backproj, loopFrame);
+		distances[loopFrame] = block.y-block.height;
+		for (int j = 0; j < backproj.cols / numCols; j++)
 		{
-			int cols = backproj.cols, rows = backproj.rows, r = (MIN(cols, rows) + 5) / 6;
-			trackWindow = Rect(trackWindow.x - r, trackWindow.y - r,
-				trackWindow.x + r, trackWindow.y + r) &
-				Rect(0, 0, cols, rows);
+			Point distLineL, distLineR;
+			distLineL.x = j*(backproj.cols/numCols); distLineL.y = distances[j];
+			distLineR.x = (j + 1)*(backproj.cols / numCols); distLineR.y = distLineL.y;
+			line(image, distLineL, distLineR, CV_RGB(255, 0, 0));
 		}
+		
 		
 		/// show windows with the results
 		imshow("CamShift Demo", image);
 		imshow("Histogram", histimg);
-		imshow("backproj", backproj);
+		imshow("backproj", ~backproj);
+
+		/// Add one to the frame we are currently calculating
+		/// for updating the distance bars in the image
+		loopFrame++;
+
+		if (loopFrame == numCols)
+			loopFrame = 0;
 
 		char c = (char)waitKey(10);
 		if (c == 27)
